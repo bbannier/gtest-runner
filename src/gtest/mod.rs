fd8b18f45d1337ba7413efaeb3bf9c9a0ac6bad0
Benjamin Bannier <bbannier@gmail.com>
2024-08-01 20:40:06 +0200

    Remove incorrect feature toggles

--- src/gtest/mod.rs
+++ src/gtest/mod.rs
@@ -1,6 +1,3 @@
-#![cfg_attr(feature = "clippy", feature(plugin))]
-#![cfg_attr(feature = "clippy", plugin(clippy))]
-
 use {
     anyhow::Result,
     console::style,
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,3 @@
-#![cfg_attr(feature = "clippy", feature(plugin))]
-#![cfg_attr(feature = "clippy", plugin(clippy))]
-
 use {
     anyhow::{anyhow, Result},
     clap::Parser,
